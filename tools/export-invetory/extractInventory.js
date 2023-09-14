36956e8d00c8218c630c6b5bdbede4201b3d85f2
Stefano Lenzi <stefano@lenzi.pro>
2023-09-14 18:28:55 +0200

    Modified so that it can be integrated with JS Injector plugin

--- tools/export-invetory/extractInventory.js
+++ tools/export-invetory/extractInventory.js
@@ -9,12 +9,6 @@ function injectLibraries(sources){
 	}
 }
 
-var scripts = [
-	"//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js",
-	"https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"
-]
-
-injectLibraries(scripts);
 
 
 /*
@@ -22,7 +16,7 @@ injectLibraries(scripts);
  */
 
 /*
- * PART 2 - start 
+ * PART 2 - start
  *
 csv = "item,qta\n";
 $('div.item').each( function (idx) {
@@ -46,11 +40,13 @@ function exportInvetoryAsCSV(){
 		return;
 	}
 	*/
-	csv = "item,qta\n";
+	csv = "item,qta,img\n";
 	$('div.item').each( function (idx) {
 	  str = $(this).find('h4').text();
 	  str += ",";
 	  str += $(this).find('b').text().replace(',','');
+		str += ",";
+		str += $(this).find('img')[0].src;
 	  csv += str + "\n";
 	})
 	var myFile = new File([csv], "invetory.csv", {type: "text/plain;charset=utf-8"});
@@ -64,9 +60,9 @@ function exportNamesAsCSV(){
 		console.log("You can download the original names only from the page "+URL);
 		return;
 	}
-	list = "CommonName,InternalName"; 
-	$('tr td a').each( function(i) { 
-		list+=$(this).text().trim()+","; 
+	list = "CommonName,InternalName";
+	$('tr td a').each( function(i) {
+		list+=$(this).text().trim()+",";
 		list += this.href.split("/").pop()+"\n"
 	});
 
@@ -74,9 +70,45 @@ function exportNamesAsCSV(){
 	saveAs(myFile);
 }
 
-x = document.createElement('div');
-x.innerHTML = '<button style="margin: 3px 5px;" class="blue-primary button"><div class="button-wrapper"><i class="fas fa-file-csv" style="margin-right: 4px;" aria-hidden="true"></i>Export to CSV</div></button>'
-x.addEventListener('click',exportInvetoryAsCSV);
+function addInventoryButton() {
+  	console.log("Inventory Button");
+	var x = document.createElement('div');
+	x.innerHTML = '<button id="x-msftools-inventory" style="margin: 3px 5px;" class="blue-primary button"><div class="button-wrapper"><i class="fas fa-file-csv" style="margin-right: 4px;" aria-hidden="true"></i>Export to CSV</div></button>'
+	x.addEventListener('click',exportInvetoryAsCSV);
 
-root = document.getElementsByClassName('player-inventory')[0];
-root.childNodes[0].appendChild(x);
+	root = document.getElementsByClassName('player-inventory')[0];
+	root.childNodes[0].appendChild(x);
+}
+
+var rebuildUIInterval = null;
+var numberOfCheck = 0;
+
+function rebuildUI(){
+  console.log("Check UI");
+	if ( jQuery('#x-msftools-inventory').length == 0 ){
+		addInventoryButton();
+      	numberOfCheck = 0;
+	}else{
+      	numberOfCheck++;
+      	if( numberOfCheck == 10 ) {
+          clearInterval(rebuildUIInterval);
+        }
+	}
+}
+
+
+jQuery(document).ready(function (){
+	/*
+	var scripts = [
+		"//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js",
+		"https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"
+	]
+	*/
+	var scripts = [
+		"https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"
+	];
+
+	injectLibraries(scripts);
+	addInventoryButton();
+	rebuildUIInterval = setInterval( rebuildUI(), 1000);
+});
